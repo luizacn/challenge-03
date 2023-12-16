167957a80ab3d7bf5efddddcbe35a7f708b1900e
Luiz abreu <luizcna2022@gmail.com>
2023-12-15 21:54:37 -0300

    created style.css

---
+++ js/script.js
@@ -0,0 +1,4 @@
+function resposta2(){
+    document.querySelector(".pergunta2").textContent = "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.";
+}
+
