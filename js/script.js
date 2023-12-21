bedc1a7e5fff18e5a22e4f310844f59cd6b1941e
Luiz abreu <luizcna2022@gmail.com>
2023-12-21 18:42:42 -0300

    modified index.html, style.css and script,js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,58 @@
-// const resposta2 = document.querySelector(".resposta2").innerHTML;
+function aparecer1() {
+    document.getElementById("resposta1").style.display = "block";
+    document.getElementById("mais1").style.display = "none";
+    document.getElementById("menos1").style.display = "block";
+}
 
-// resposta2.innerHTML = resposta2;
+function desaparecer1() {
+    document.getElementById("resposta1").style.display = "none";
 
+    document.getElementById("mais1").style.display = "block";
+    document.getElementById("menos1").style.display = "none";
 
-function aparecer() {
+}
+
+function aparecer2() {
     document.getElementById("resposta2").style.display = "block";
     document.getElementById("mais2").style.display = "none";
     document.getElementById("menos2").style.display = "block";
 }
 
-function desaparecer() {
+function desaparecer2() {
     document.getElementById("resposta2").style.display = "none";
+
     document.getElementById("mais2").style.display = "block";
     document.getElementById("menos2").style.display = "none";
+
 }
 
+function aparecer3() {
+    document.getElementById("resposta3").style.display = "block";
+    document.getElementById("mais3").style.display = "none";
+    document.getElementById("menos3").style.display = "block";
+}
+
+function desaparecer3() {
+    document.getElementById("resposta3").style.display = "none";
+
+    document.getElementById("mais3").style.display = "block";
+    document.getElementById("menos3").style.display = "none";
+
+}
+
+function aparecer4() {
+    document.getElementById("resposta4").style.display = "block";
+    document.getElementById("mais4").style.display = "none";
+    document.getElementById("menos4").style.display = "block";
+}
+
+function desaparecer4() {
+    document.getElementById("resposta4").style.display = "none";
+
+    document.getElementById("mais4").style.display = "block";
+    document.getElementById("menos4").style.display = "none";
+
+}
 
 
 
