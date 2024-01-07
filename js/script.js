e163fb517fa1bd1dfa6564206f03d7e51ba409ee
Luiz abreu <luizcna2022@gmail.com>
2024-01-07 16:16:04 -0300

    update index.html, style.css e script.js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,70 @@
-//a função resposta1() chama a função aparecer1(), uma função dentro da outra
-// function resposta1(){ 
-//     aparecer1();
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener("click", () => {
+        faq.classList.toggle("active");
+    });
+});
+
+
+
+
+// //a função resposta1() chama a função aparecer1(), uma função dentro da outra
+// // function resposta1(){ 
+// //     aparecer1();
+// // }
+
+// function aparecer1() {
+//     document.getElementById("resposta1").style.display = "block";
+//     document.getElementsByClassName("mais1").style.display = "none";
+//     document.getElementById("menos1").style.display = "block";
 // }
 
-function aparecer1() {
-    document.getElementById("resposta1").style.display = "block";
-    document.getElementsByClassName("mais1").style.display = "none";
-    document.getElementById("menos1").style.display = "block";
-}
+// function desaparecer1() {
+//     document.getElementById("resposta1").style.display = "none";
+//     document.getElementById("mais1").style.display = "block";
+//     document.getElementById("menos1").style.display = "none";
 
-function desaparecer1() {
-    document.getElementById("resposta1").style.display = "none";
-    document.getElementById("mais1").style.display = "block";
-    document.getElementById("menos1").style.display = "none";
+// }
 
-}
+// function aparecer2() {
+//     document.getElementById("resposta2").style.display = "block";
+//     document.getElementById("mais2").style.display = "none";
+//     document.getElementById("menos2").style.display = "block";
+// }
 
-function aparecer2() {
-    document.getElementById("resposta2").style.display = "block";
-    document.getElementById("mais2").style.display = "none";
-    document.getElementById("menos2").style.display = "block";
-}
+// function desaparecer2() {
+//     document.getElementById("resposta2").style.display = "none";
+//     document.getElementById("mais2").style.display = "block";
+//     document.getElementById("menos2").style.display = "none";
 
-function desaparecer2() {
-    document.getElementById("resposta2").style.display = "none";
-    document.getElementById("mais2").style.display = "block";
-    document.getElementById("menos2").style.display = "none";
+// }
 
-}
+// function aparecer3() {
+//     document.getElementById("resposta3").style.display = "block";
+//     document.getElementById("mais3").style.display = "none";
+//     document.getElementById("menos3").style.display = "block";
+// }
 
-function aparecer3() {
-    document.getElementById("resposta3").style.display = "block";
-    document.getElementById("mais3").style.display = "none";
-    document.getElementById("menos3").style.display = "block";
-}
+// function desaparecer3() {
+//     document.getElementById("resposta3").style.display = "none";
+//     document.getElementById("mais3").style.display = "block";
+//     document.getElementById("menos3").style.display = "none";
 
-function desaparecer3() {
-    document.getElementById("resposta3").style.display = "none";
-    document.getElementById("mais3").style.display = "block";
-    document.getElementById("menos3").style.display = "none";
+// }
 
-}
+// function aparecer4() {
+//     document.getElementById("resposta4").style.display = "block";
+//     document.getElementById("mais4").style.display = "none";
+//     document.getElementById("menos4").style.display = "block";
+// }
 
-function aparecer4() {
-    document.getElementById("resposta4").style.display = "block";
-    document.getElementById("mais4").style.display = "none";
-    document.getElementById("menos4").style.display = "block";
-}
+// function desaparecer4() {
+//     document.getElementById("resposta4").style.display = "none";
+//     document.getElementById("mais4").style.display = "block";
+//     document.getElementById("menos4").style.display = "none";
 
-function desaparecer4() {
-    document.getElementById("resposta4").style.display = "none";
-    document.getElementById("mais4").style.display = "block";
-    document.getElementById("menos4").style.display = "none";
-
-}
+// }
 
 
 
